1c58faf8d25f34efb0b76a05b4770b214f45a3d6
kbjdev <qudwnbj.dev@gmail.com>
2024-08-22 16:44:22 +0900

    feat(react-vscode): vscode theme color

--- packages/react-vscode/.eslintrc.js
+++ packages/react-vscode/.eslintrc.js
@@ -1,5 +1,10 @@
 require('@kbjdev/eslint-config/patch');
 module.exports = {
+  settings: {
+    react: {
+      version: '18.3.1',
+    },
+  },
   extends: ['@kbjdev/eslint-config', '@kbjdev/eslint-config/mixins/react'],
   parserOptions: { project: true, tsconfigRootDir: __dirname },
   rules: {
@@ -7,10 +12,7 @@ module.exports = {
       'warn',
       {
         alphabetize: { order: 'asc' },
-        pathGroups: [
-          { pattern: '@src/**', group: 'internal' },
-          { pattern: 'react', group: 'external', position: 'before' },
-        ],
+        pathGroups: [{ pattern: 'react', group: 'external', position: 'before' }],
         pathGroupsExcludedImportTypes: ['react'],
         groups: ['builtin', 'external', 'internal', 'sibling', 'parent', 'index', 'type'],
       },
